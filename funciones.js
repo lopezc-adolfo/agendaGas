6a3053d3498e418fa7258d7e023f24888511551d
Adolfo López <indata.inteligencia@gmail.com>
2023-05-11 18:54:38 -0500

    Avance clase 9: función obtenerContactos y configuración de archivo main.html

--- funciones.js
+++ funciones.js
@@ -7,3 +7,9 @@ function doGet() {
 function obtenerDatosHTML(nombre) {
    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+ let hoja = SpreadsheetApp.openById('14COXrGrCi0htxdSg2zDa8shcky8F3pltw8tGbNjvySs').getSheetByName('AGENDA');
+ let datos = hoja.getDataRange().getValues();
+ return datos;
+}
